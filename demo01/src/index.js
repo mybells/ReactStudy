1b809792af709582b0d7432178fbe667b0069e29
mybells <435541908@qq.com>
2020-04-22 09:05:16 +0800

    受控组件

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
 
+//input输入值不可被修改，指定为null或者为空则可以修改
+// ReactDOM.render(<input value="hi" />, document.getElementById('root'));
+
 ReactDOM.render(<TodoList />,document.getElementById('root'));
